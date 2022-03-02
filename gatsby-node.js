22b54f8c64542f8521308103c10177b117ae811f
Neil McDonald <w0225318@campus.nscc.ca>
2022-03-02 01:38:10 -0400

    jkj

--- gatsby-node.js
+++ gatsby-node.js
@@ -32,22 +32,22 @@ exports.createPages = async ({ graphql, actions }) => {
   }
 
   // Create blog posts pages.
-  const posts = result.data.allMarkdownRemark.edges;
+  // const posts = result.data.allMarkdownRemark.edges;
 
-  posts.forEach((post, index) => {
-    const previous = index === posts.length - 1 ? null : posts[index + 1].node;
-    const next = index === 0 ? null : posts[index - 1].node;
+  // posts.forEach((post, index) => {
+  //   const previous = index === posts.length - 1 ? null : posts[index + 1].node;
+  //   const next = index === 0 ? null : posts[index - 1].node;
 
-    createPage({
-      path: post.node.fields.slug,
-      component: blogPost,
-      context: {
-        slug: post.node.fields.slug,
-        previous,
-        next,
-      },
-    });
-  });
+  //   createPage({
+  //     path: post.node.fields.slug,
+  //     component: blogPost,
+  //     context: {
+  //       slug: post.node.fields.slug,
+  //       previous,
+  //       next,
+  //     },
+  //   });
+  // });
 };
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
