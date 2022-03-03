7521e80f02dedae620c8a657baa7d25bddfcba76
Neil McDonald <w0225318@campus.nscc.ca>
2022-03-03 13:42:49 -0400

    fdsaf

--- src/components/blog-posts/index.jsx
+++ src/components/blog-posts/index.jsx
@@ -1,22 +1,22 @@
-import React from 'react';
+// import React from 'react';
 
-import Section from '../section';
-import SummaryItem from '../summary-item';
+// import Section from '../section';
+// import SummaryItem from '../summary-item';
 
-const BlogPosts = ({ posts }) => {
-  return (
-    <Section title="All Blog Posts">
-      {posts.map((post) => (
-        <SummaryItem
-          key={post.node.fields.slug}
-          name={post.node.frontmatter.title}
-          description={post.node.frontmatter.description}
-          link={post.node.fields.slug}
-          internal
-        />
-      ))}
-    </Section>
-  );
-};
+// const BlogPosts = ({ posts }) => {
+//   return (
+//     <Section title="All Blog Posts">
+//       {posts.map((post) => (
+//         <SummaryItem
+//           key={post.node.fields.slug}
+//           name={post.node.frontmatter.title}
+//           description={post.node.frontmatter.description}
+//           link={post.node.fields.slug}
+//           internal
+//         />
+//       ))}
+//     </Section>
+//   );
+// };
 
-export default BlogPosts;
+// export default BlogPosts;
