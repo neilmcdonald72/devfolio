8812bf6a501d754990fda7b5f8511bbae5564ccf
Neil McDonald <w0225318@campus.nscc.ca>
2022-03-03 13:40:44 -0400

    fads

--- gatsby-node.js
+++ gatsby-node.js
@@ -27,28 +27,28 @@
 //     `
 //   );
 
-  if (result.errors) {
-    throw result.errors;
-  }
+//   if (result.errors) {
+//     throw result.errors;
+//   }
 
-  // Create blog posts pages.
-  const posts = result.data.allMarkdownRemark.edges;
+//   // Create blog posts pages.
+//   const posts = result.data.allMarkdownRemark.edges;
 
-  posts.forEach((post, index) => {
-    const previous = index === posts.length - 1 ? null : posts[index + 1].node;
-    const next = index === 0 ? null : posts[index - 1].node;
+//   posts.forEach((post, index) => {
+//     const previous = index === posts.length - 1 ? null : posts[index + 1].node;
+//     const next = index === 0 ? null : posts[index - 1].node;
 
-    createPage({
-      path: post.node.fields.slug,
-      component: blogPost,
-      context: {
-        slug: post.node.fields.slug,
-        previous,
-        next,
-      },
-    });
-  });
-};
+//     createPage({
+//       path: post.node.fields.slug,
+//       component: blogPost,
+//       context: {
+//         slug: post.node.fields.slug,
+//         previous,
+//         next,
+//       },
+//     });
+//   });
+// };
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions;
